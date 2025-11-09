018ffc05ede7aef10055f4c6be9e3d908d25a5b9
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2025-11-09 20:56:47 +0300

    error in index.css

--- src/App.js
+++ src/App.js
@@ -3,23 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold underline text-blue-400">
+        Tailwind is working! 🚀
+      </h1>
     </div>
   );
 }
 
 export default App;
+
